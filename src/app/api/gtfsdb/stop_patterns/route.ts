9f34fbccf82dfa7263b3f1b8c3327d1e6875369f
swallow3 <YamateMokei@users.noreply.github.com>
2025-03-31 13:20:42 +0900

    使わないapiの削除

--- src/app/api/gtfsdb/stop_patterns/route.ts
+++ src/app/api/gtfsdb/stop_patterns/route.ts
@@ -1,48 +1,51 @@
-import { API } from "@/app/api/common/api";
-import db from "@/app/api/gtfsdb/db";
+// import { API } from "@/app/api/common/api";
+// import db from "@/app/api/gtfsdb/db";
+// import { NextResponse } from "next/server";
+
 import { NextResponse } from "next/server";
 
-const routesAPI = new API({
-  endpoint: 'gtfsdb/stop_patterns',
-  reqObjDef: {
-    feed_id: 'number',
-    trip_id: ['string'],
-    stop_id: ['string'],
-  },
+// const routesAPI = new API({
+//   endpoint: 'gtfsdb/stop_patterns',
+//   reqObjDef: {
+//     feed_id: 'number',
+//     trip_id: ['string'],
+//     stop_id: ['string'],
+//   },
 
-  async getProcesor(reqObj) {
-    const {
-      feed_id: feedId,
-      trip_id: tripIds,
-      stop_id: stopIds
-    } = reqObj;
+//   async getProcesor(reqObj) {
+//     const {
+//       feed_id: feedId,
+//       trip_id: tripIds,
+//       stop_id: stopIds
+//     } = reqObj;
 
-    const result = await db.run(`
-      select
-        stop_patterns.feed_id,
-        trips.trip_id,
-        stop_patterns.pattern_id,
-        stop_patterns.route_name,
-        stop_patterns.route_type,
-        stop_patterns.stop_sequence,
-        stop_patterns.stop_id,
-        stop_patterns.stop_name,
-        stop_patterns.stop_headsign,
-        stop_patterns.platform_code
-      from trips
-      inner join stop_patterns using(feed_id, pattern_id)
-      where
-        trips.feed_id = $1 and
-        (trips.trip_id, stop_patterns.stop_id) in (${tripIds.map((s, i) => `($${i + 2}, $${i + tripIds.length + 2})`).join(', ')})
+//     const result = await db.run(`
+//       select
+//         stop_patterns.feed_id,
+//         trips.trip_id,
+//         stop_patterns.pattern_id,
+//         stop_patterns.route_name,
+//         stop_patterns.route_type,
+//         stop_patterns.stop_sequence,
+//         stop_patterns.stop_id,
+//         stop_patterns.stop_name,
+//         stop_patterns.stop_headsign,
+//         stop_patterns.platform_code
+//       from trips
+//       inner join stop_patterns using(feed_id, pattern_id)
+//       where
+//         trips.feed_id = $1 and
+//         (trips.trip_id, stop_patterns.stop_id) in (${tripIds.map((s, i) => `($${i + 2}, $${i + tripIds.length + 2})`).join(', ')})
       
-      `, [feedId, ...tripIds, ...stopIds]
-    );
-    if (!result) return NextResponse.json([]);
+//       `, [feedId, ...tripIds, ...stopIds]
+//     );
+//     if (!result) return NextResponse.json([]);
     
-    return NextResponse.json(result);
-  },
-});
+//     return NextResponse.json(result);
+//   },
+// });
 
-export async function GET(request: Request) {
-  return await routesAPI.get(request);
+export async function GET() {
+  return NextResponse.json({ stop_patterns: "not implemented" });
+  // return await routesAPI.get(request);
 };
--- src/app/api/gtfsdb/stop_times/route.ts
+++ src/app/api/gtfsdb/stop_times/route.ts
@@ -1,43 +1,46 @@
-import { API } from "@/app/api/common/api";
-import db from "@/app/api/gtfsdb/db";
+// import { API } from "@/app/api/common/api";
+// import db from "@/app/api/gtfsdb/db";
+// import { NextResponse } from "next/server";
+
 import { NextResponse } from "next/server";
 
-const routesAPI = new API({
-  endpoint: 'gtfsdb/stop_times',
-  reqObjDef: {
-    feed_id: 'number',
-    trip_id: 'string'
-  },
+// const routesAPI = new API({
+//   endpoint: 'gtfsdb/stop_times',
+//   reqObjDef: {
+//     feed_id: 'number',
+//     trip_id: 'string'
+//   },
 
-  async getProcesor(reqObj) {
-    const result = await db.run(`
-      SELECT 
-        feed_id,
-        trip_id,
-        stop_sequence,
-        stop_id,
-        arrival_time,
-        departure_time,
-        stop_headsign,
-        pickup_type,
-        drop_off_type,
-        stop_name,
-        platform_code
-      FROM stop_times
-      inner join stops using (feed_id, stop_id)
-      WHERE feed_id = $1 and trip_id = $2
+//   async getProcesor(reqObj) {
+//     const result = await db.run(`
+//       SELECT 
+//         feed_id,
+//         trip_id,
+//         stop_sequence,
+//         stop_id,
+//         arrival_time,
+//         departure_time,
+//         stop_headsign,
+//         pickup_type,
+//         drop_off_type,
+//         stop_name,
+//         platform_code
+//       FROM stop_times
+//       inner join stops using (feed_id, stop_id)
+//       WHERE feed_id = $1 and trip_id = $2
       
-      order by stop_sequence
-      `,
-      [reqObj.feed_id, reqObj.trip_id]
-    );
+//       order by stop_sequence
+//       `,
+//       [reqObj.feed_id, reqObj.trip_id]
+//     );
 
-    if (!result) return NextResponse.json([]);
+//     if (!result) return NextResponse.json([]);
     
-    return NextResponse.json(result);
-  },
-});
+//     return NextResponse.json(result);
+//   },
+// });
 
-export async function GET(request: Request) {
-  return await routesAPI.get(request);
+export async function GET() {
+  return NextResponse.json({ stop_times: "not implemented" });
+  // return await routesAPI.get(request);
 };
